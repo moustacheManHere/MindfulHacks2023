aa31db5d7a85e5af4f1710ee41d44b48bc230199
SchwiDola-exe <83574147+SchwiDola-exe@users.noreply.github.com>
2023-10-07 09:40:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
-st.write("# benj gay")
+# Title of the app
+st.title('Mental Health App')
+
+# Introduction
+st.write("Welcome to the Mental Health App. This is a safe space where you can find resources, take self-assessments, and learn more about mental health.")
+
+# Navigation
+page = st.sidebar.selectbox("Choose a page", ["Home", "Resources", "Self-Assessment"])
+
+if page == "Home":
+    st.header("Home")
+    st.write("This is the home page.")
+elif page == "Resources":
+    st.header("Resources")
+    st.write("This is the resources page.")
+elif page == "Self-Assessment":
+    st.header("Self-Assessment")
+    st.write("This is the self-assessment page.")
+    
